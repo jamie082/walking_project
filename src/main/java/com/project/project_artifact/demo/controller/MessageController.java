557b143df6807e5b2239448c3785d53e3f7d93fa
Jamie Morrissey <jmo1344@wgu.edu>
2025-07-29 14:54:00 -0700

    Second commit

---
+++ src/main/java/com/project/project_artifact/demo/controller/MessageController.java
@@ -0,0 +1,23 @@
+package com.project.project_artifact.demo.controller;
+import com.project.project_artifact.demo.model.Message;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+public class MessageController {
+
+    @GetMapping("/")
+    public String showForm(Model model) {
+        model.addAttribute("message", new Message());
+        return "form";
+    }
+
+    @PostMapping("/submit")
+    public String submitForm(@ModelAttribute Message message, Model model) {
+        model.addAttribute("submittedMessage", message.getContent());
+        return "result";
+    }
+}
