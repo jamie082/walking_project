8c772ebda9b0b6cd68193904ae06a999c862466e
Jamie Morrissey <jmo1344@wgu.edu>
2025-07-29 14:57:56 -0700

    edited

--- src/main/java/com/project/project_artifact/demo/controller/MessageController.java
+++ src/main/java/com/project/project_artifact/demo/controller/MessageController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
+@Controller
 public class MessageController {
 
     @GetMapping("/")
